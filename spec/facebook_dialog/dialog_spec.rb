bea0497ee1b8b8a1ade379d62b5badd7d039aaad
Dan Pickett <dpickett@enlightsolutions.com>
2011-12-20 17:02:21 -0500

    assertion to ensure app_id is set

--- spec/facebook_dialog/dialog_spec.rb
+++ spec/facebook_dialog/dialog_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 describe FacebookDialog::Dialog do
   class DummyDialog < FacebookDialog::Dialog; end
 
+  it "includes the api key as an app id argument" do
+    FacebookDialog.api_key = "something"
+    FacebookDialog::Feed.new.url.should include("app_id=#{Rack::Utils.escape(FacebookDialog.api_key)}")
+  end
+  
   it "raises an error when a resource name is not defined when getting base uri" do
     lambda { DummyDialog.base_uri }.should raise_error(
       FacebookDialog::ResourceNameNotDefined
